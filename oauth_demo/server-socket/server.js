4a605e22996a6d65520159476f0ffdba5b3beb29
sostenesfreitas <sostenesfreitas@gmail.com>
2017-05-26 15:49:12 +0000

    vai

--- oauth_demo/server-socket/server.js
+++ oauth_demo/server-socket/server.js
@@ -8,12 +8,12 @@ const app       = express();
 const morgan    = require('morgan');
 const http      = require('http').Server(app);
 const io        = require('socket.io')(http);
-const port      = process.env.port | 8081;
+const port      = process.env.port | 8082;
 app.use(morgan('dev')); // Log HTTP information - 200, 404, 503 etc... Good for development.
 //app.use(express.static('public')); // Use static files from client
 
 //require('./server/routes.js')(app); // Include routes file
-require('./server/sockets.js')(io); // Include sockets file
+require('./sockets.js')(io); // Include sockets file
 
 // Start server
 http.listen(port, function() {
--- oauth_demo/server-socket/sockets.js
+++ oauth_demo/server-socket/sockets.js
@@ -2,23 +2,40 @@
  * @version 1.0.0
  * @description Creating a module to break our application up. This module is used to deal with Socket.io, connections and messages. Required in server.js
  * @author Aaron Welsh <contact@aaron-welsh.co.uk>
- * 
+ *
  */
-module.exports  = function(io) {
+module.exports  = io => {
     // Handle socket.io connections
-    io.on('connection', function(socket) {
+    io.on('connection', socket => {
 
         //log if user joined
         console.log('a user connected');
-        socket.on('disconnect', function() {
+        socket.on('disconnect', () => {
             //log if user disconnected
             console.log('user disconnected');
         });
 
         // Handle message sent
-        socket.on('listenForMessage', function(message) {
+        socket.on('listenForMessage', message => {
             console.log('Message: ' + message);
             io.emit('listenForMessage', message);
+        });
+        // Indicadores
+        socket.on('Medicamento', medicamento => {
+            console.log('Medicamento: ' + medicamento);
+            io.emit('Medicamento', medicamento);
+        });
+        socket.on('Meta', meta => {
+            console.log('Meta: ' + meta);
+            io.emit('Meta', meta);
+        });
+        socket.on('listenForMedicine', medicamento => {
+            console.log('listenForMedicine: ' + medicamento);
+            io.emit('listenForMedicine', medicamento);
+        });
+        socket.on('listenForMetas', meta => {
+            console.log('listenForMetas: ' + meta);
+            io.emit('listenForMetas', meta);
         });
     });
 };
